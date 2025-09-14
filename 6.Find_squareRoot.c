d0045e5dd3eeb5479c363316f09a58e5b4075e20
masumb2k2 <masumb2k2@gmail.com>
2025-09-14 15:32:56 +0600

    6.Find_squareRoot

---
+++ 6.Find_squareRoot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int num;
+    printf("Enter a number :");
+    scanf("%d",&num);
+    printf("Square Root value: %f",sqrt(num));
+    return 0;
+
+}
