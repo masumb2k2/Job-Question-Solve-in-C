37b8555427cf5cb706171a2b480f1b2bac32bbff
masumb2k2 <masumb2k2@gmail.com>
2025-09-14 19:31:53 +0600

    12.Area_of_cube

---
+++ 12.Area_of_cube.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float a,area;
+    printf("Enter Lenth of Arm: ");
+    scanf("%f",&a);
+    area=a*a*a;
+    printf("Volume of Cube: %.2f",area);
+    return 0;
+}
